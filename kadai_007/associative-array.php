de19753242af0020ae9556a79a2919d6a5bdf7b7
y0n888 <ayam.k_123@outlook.jp>
2024-08-21 16:23:12 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+ 
+<head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+</head>
+ 
+<body>
+     <p>
+        <?php
+         $Array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+         print_r($Array);
+        ?>
+     </p>
+</body>
+ 
+</html>
